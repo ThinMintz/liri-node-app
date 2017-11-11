f407565db3e89484324c547c0f80dc3141e3c9fa
ThinMintz <rachel.rodriguez05@gmail.com>
2017-11-11 14:56:03 -0600

    first push

---
+++ assets/key.js
@@ -0,0 +1,33 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'oRSlzcmQc6rbMBGIicbcDoVWU',
+  consumer_secret: 'ewCX5IC7Gyhnr568p00xRCr6lE8KVgbIaXPZgkRgcVc07u9TB9',
+  access_token_key: '926993449798307840-vnuphi3xksiJbPaHvNqHW4Mqoe1fsDB',
+  access_token_secret: 'xsIzYM378QcJ077dqkM5D8PlADDQudK9Y2uY1ZogPNgVT',
+}
+
+module.exports = twitterKeys;
+
+var spotifyKeys = {
+  consumer_key: '<input here2>',
+  consumer_secret: '<input here>',
+  access_token_key: '<input here>',
+  access_token_secret: '<input here>',
+}
+
+module.exports = spotifyKeys;
+
+var omdbKeys = {
+  consumer_key: '<input here3>',
+  consumer_secret: '<input here>',
+  access_token_key: '<input here>',
+  access_token_secret: '<input here>',
+}
+
+module.exports = omdbKeys;
+
+Client ID
+9610926d69244ec4b4a8d01e0cfbbd82
+Client Secret
+96164f81ac5a4aa3a47cafeb9aa32aa7
---
+++ liri.js
@@ -0,0 +1,7 @@
+var twitterKeys = require("./assets/key.js");
+var spotifyKeys = require("./assets/key.js");
+var omdbKeys = require("./assets/key.js");
+
+/*for (var key in twitterKeys) {
+	console.log(twitterKeys[key]);
+}*/
