e165f808e5c32677d1084c85d1a36fb545337e02
ThinMintz <rachel.rodriguez05@gmail.com>
2017-11-12 14:06:13 -0600

    got movie-this working

--- assets/key.js
+++ assets/key.js
@@ -10,24 +10,10 @@ var twitterKeys = {
 module.exports = twitterKeys;
 
 var spotifyKeys = {
-  consumer_key: '<input here2>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: '9610926d69244ec4b4a8d01e0cfbbd82',
+  consumer_secret: '96164f81ac5a4aa3a47cafeb9aa32aa7',
 }
 
 module.exports = spotifyKeys;
 
-var omdbKeys = {
-  consumer_key: '<input here3>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
-}
 
-module.exports = omdbKeys;
-
-Client ID
-9610926d69244ec4b4a8d01e0cfbbd82
-Client Secret
-96164f81ac5a4aa3a47cafeb9aa32aa7
--- liri.js
+++ liri.js
@@ -1,46 +1,36 @@
-/*var twitterKeys = require("./assets/key.js");
+var twitterKeys = require("./assets/key.js");
 var spotifyKeys = require("./assets/key.js");
-var omdbKeys = require("./assets/key.js");*/
+var omdbKeys = require("./assets/key.js");
 var request = require("request");
-/*var inquirer = require("inquirer");
 var twitter = require ("twitter");
-var omdb = require("omdb");
 var spotify = require("spotify-web-api-node");
-var userInput = parse.argv[2];
-var tweets = 
-var thisSong = 
-var moviePick
-var whatItSays*/
+var userCommands = process.argv[2];
+var userRequests = process.argv[3];
 
 
-CAN't GET WORKNG! :(
+/*CAN't GET WORKNG! :(*/
 //different commands
-/*switch(commands){
+switch(userCommands){
     case 'my-tweets':
         tweets();
         break;
-
     case 'spotify-this-song':
         song();
         break;
-
     case 'movie-this':
         movie();
         break;
-
     case 'do-what-it-says':
         doSomething();
         break;
-}*/
+}
 
 //runs the "movie-this" function
 function movie() {
 //Store all of the arguments in an array
 var nodeArgs = process.argv;
-
 //Create an empty variable for holding the movie name
 var movieName = "";
-
 //Loop through all the words in the node argument (use a for loop for inclusion)
 for (var i = 3; i < nodeArgs.length; i++) {
 	if(i > 3 && i < nodeArgs.length) {
@@ -49,13 +39,10 @@ for (var i = 3; i < nodeArgs.length; i++) {
 		movieName += nodeArgs[i];
 	}
 }  
-
 // We then run the request module on a URL with a JSON
 var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
-
 request(queryUrl, function(error, response, body) {
   if (!error && response.statusCode === 200) {
-
     // Then we print out the imdbRating
     console.log("Title of the movie: " + JSON.parse(body).Title);
     console.log("Year Released: " + JSON.parse(body).Year);
@@ -69,7 +56,6 @@ request(queryUrl, function(error, response, body) {
 });	
 };
   // If there were no errors and the response code was 200 (i.e. the request was successful)...
-
 /*
    * Title of the movie.
    * Year the movie came out.
@@ -79,7 +65,6 @@ request(queryUrl, function(error, response, body) {
    * Language of the movie.
    * Plot of the movie.
    * Actors in the movie.*/
-
 /*inquirer.prompt([
 {
     type: 'list',
@@ -91,3 +76,5 @@ request(queryUrl, function(error, response, body) {
       'Some information about a movie?',
     ]
   }]);*/
+
+
