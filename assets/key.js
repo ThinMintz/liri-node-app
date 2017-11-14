7eb489b9b95137116e4fd19393da972f0495ec36
ThinMintz <rachel.rodriguez05@gmail.com>
2017-11-13 19:02:42 -0600

    still working on getting OMDB working properly

--- assets/key.js
+++ assets/key.js
@@ -15,5 +15,3 @@ var spotifyKeys = {
 }
 
 module.exports = spotifyKeys;
-
-
--- liri.js
+++ liri.js
@@ -1,46 +1,44 @@
-var twitterKeys = require("./assets/key.js");
-var spotifyKeys = require("./assets/key.js");
-var omdbKeys = require("./assets/key.js");
+/*var twitterKeys = require("./assets/key.js");*/
+/*var spotifyKeys = require("./assets/key.js");*/
+
 var request = require("request");
-var twitter = require ("twitter");
-var spotify = require("spotify-web-api-node");
+/*var twitter = require ("twitter");
+var spotify = require("spotify-web-api-node");*/
 var userCommands = process.argv[2];
 var userRequests = process.argv[3];
-
+var searchThis = "";
+//MAKE FOR LOOP A GLOBAL variable (so it will loop through songs as well)
+for (var i = 3; i < userRequests.length; i++) {
+  searchThis += " " + userRequests[i]};
 
 /*CAN't GET WORKNG! :(*/
 //different commands
 switch(userCommands){
-    case 'my-tweets':
+/*    case 'my-tweets':
         tweets();
         break;
     case 'spotify-this-song':
         song();
-        break;
+        break;*/
     case 'movie-this':
         movie();
         break;
-    case 'do-what-it-says':
+/*    case 'do-what-it-says':
         doSomething();
-        break;
+        break;*/
 }
 
 //runs the "movie-this" function
 function movie() {
 //Store all of the arguments in an array
-var nodeArgs = process.argv;
+// var userRequest = process.argv;
 //Create an empty variable for holding the movie name
-var movieName = "";
+/*var movieName = "";*/
+
 //Loop through all the words in the node argument (use a for loop for inclusion)
-for (var i = 3; i < nodeArgs.length; i++) {
-	if(i > 3 && i < nodeArgs.length) {
-		movieName = movieName + "+" + nodeArgs[i];
-	} else {
-		movieName += nodeArgs[i];
-	}
-}  
+ 
 // We then run the request module on a URL with a JSON
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+var queryUrl = "http://www.omdbapi.com/?t=" + userRequests + "&y=&plot=short&tomatoes=true&apikey=40e9cece";
 request(queryUrl, function(error, response, body) {
   if (!error && response.statusCode === 200) {
     // Then we print out the imdbRating
@@ -52,6 +50,9 @@ request(queryUrl, function(error, response, body) {
     console.log("Movies Language: " + JSON.parse(body).Language);
     console.log("Plot of the Movie: "  + JSON.parse(body).Plot);
     console.log("This movie stars: "  + JSON.parse(body).Actors);
+  }
+  else{
+  	console.log(error);
   }
 });	
 };
@@ -76,5 +77,24 @@ request(queryUrl, function(error, response, body) {
       'Some information about a movie?',
     ]
   }]);*/
-
-
+  // If there were no errors and the response code was 200 (i.e. the request was successful)...
+/*
+   * Title of the movie.
+   * Year the movie came out.
+   * IMDB Rating of the movie.
+   * Rotten Tomatoes Rating of the movie.
+   * Country where the movie was produced.
+   * Language of the movie.
+   * Plot of the movie.
+   * Actors in the movie.*/
+/*inquirer.prompt([
+{
+    type: 'list',
+    name: 'theme',
+    message: 'What would you like to see?',
+    choices: [
+      'My last tweets?',
+      'Song information?',
+      'Some information about a movie?',
+    ]
+  }]);*/
