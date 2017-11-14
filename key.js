49b80f207da5fd030e3554236aefae31e468f5e6
ThinMintz <rachel.rodriguez05@gmail.com>
2017-11-13 21:10:19 -0600

    got twitter working, but couldn't get my code working, kept getting error89

---
+++ key.js
@@ -0,0 +1,26 @@
+console.log('this is loaded');
+
+/*var twitterKeys = {
+  consumer_key: 'E400QaXuOI3ZUBJDhlNdJCjZH',
+  consumer_secret: '0weszRspC4RKpwAr883fTMAkMcC8DYU2LNTRpZhDwNw2wczld2',
+  access_token_key: '926993449798307840-gMwANci8oNCZlqCYiGWMmicfy4MqJHg',
+  access_token_secret: 'AcbDFXhdhv7ciCRDuo5DU1Ll2unHBs1AnrYdUFhQY2HaU',
+}*/
+
+var twitterKeys = {
+  consumer_key: 'D57jHbtqbQMii2d0pnDRNx9bo',
+  consumer_secret: 'vbNt6vfcP9jxHcEF2e5bbunZumAxFzqf4ildxCN3vsIW78L5fG',
+  access_token_key: '559262783-UmBGvywk7qUQ99Yd2OLKtJ7B8dprvL4JG1koXN9q',
+  access_token_secret: 'WEHmfrGUya8mJHdMLsjOsvjfkY0sOI6wtsLATuJ5V0tNF',
+}
+
+
+var spotifyKeys = {
+  consumer_key: '9610926d69244ec4b4a8d01e0cfbbd82',
+  consumer_secret: '96164f81ac5a4aa3a47cafeb9aa32aa7',
+}
+
+module.exports = {
+  spotifyKeys: spotifyKeys,
+  twitterKeys: twitterKeys
+}
--- liri.js
+++ liri.js
@@ -1,44 +1,36 @@
-/*var twitterKeys = require("./assets/key.js");*/
-/*var spotifyKeys = require("./assets/key.js");*/
-
+var key = require("./key.js");
 var request = require("request");
-/*var twitter = require ("twitter");
-var spotify = require("spotify-web-api-node");*/
+var Twitter = require ("twitter");
+var spotify = require("spotify-web-api-node");
 var userCommands = process.argv[2];
-var userRequests = process.argv[3];
-var searchThis = "";
-//MAKE FOR LOOP A GLOBAL variable (so it will loop through songs as well)
-for (var i = 3; i < userRequests.length; i++) {
-  searchThis += " " + userRequests[i]};
+var userRequests = process.argv.slice(3).join(' ');
+var twitKeys = new Twitter(key.twitterKeys);
+var spotKeys = new spotify(key.spotifyKeys);
+var param = {screen_name: 'JOonhisJOB'};
+var fs = require("fs");
 
-/*CAN't GET WORKNG! :(*/
 //different commands
 switch(userCommands){
-/*    case 'my-tweets':
+    case 'my-tweets':
         tweets();
         break;
     case 'spotify-this-song':
         song();
-        break;*/
+        break;
     case 'movie-this':
         movie();
         break;
-/*    case 'do-what-it-says':
+   case 'do-what-it-says':
         doSomething();
-        break;*/
+        break;
 }
 
 //runs the "movie-this" function
 function movie() {
-//Store all of the arguments in an array
-// var userRequest = process.argv;
-//Create an empty variable for holding the movie name
-/*var movieName = "";*/
-
-//Loop through all the words in the node argument (use a for loop for inclusion)
  
 // We then run the request module on a URL with a JSON
 var queryUrl = "http://www.omdbapi.com/?t=" + userRequests + "&y=&plot=short&tomatoes=true&apikey=40e9cece";
+console.log(queryUrl);
 request(queryUrl, function(error, response, body) {
   if (!error && response.statusCode === 200) {
     // Then we print out the imdbRating
@@ -56,45 +48,24 @@ request(queryUrl, function(error, response, body) {
   }
 });	
 };
-  // If there were no errors and the response code was 200 (i.e. the request was successful)...
-/*
-   * Title of the movie.
-   * Year the movie came out.
-   * IMDB Rating of the movie.
-   * Rotten Tomatoes Rating of the movie.
-   * Country where the movie was produced.
-   * Language of the movie.
-   * Plot of the movie.
-   * Actors in the movie.*/
-/*inquirer.prompt([
-{
-    type: 'list',
-    name: 'theme',
-    message: 'What would you like to see?',
-    choices: [
-      'My last tweets?',
-      'Song information?',
-      'Some information about a movie?',
-    ]
-  }]);*/
-  // If there were no errors and the response code was 200 (i.e. the request was successful)...
-/*
-   * Title of the movie.
-   * Year the movie came out.
-   * IMDB Rating of the movie.
-   * Rotten Tomatoes Rating of the movie.
-   * Country where the movie was produced.
-   * Language of the movie.
-   * Plot of the movie.
-   * Actors in the movie.*/
-/*inquirer.prompt([
-{
-    type: 'list',
-    name: 'theme',
-    message: 'What would you like to see?',
-    choices: [
-      'My last tweets?',
-      'Song information?',
-      'Some information about a movie?',
-    ]
-  }]);*/
+
+function tweets() {
+	twitKeys.get("statuses/user_timeline", param, function(error, tweets, response) {
+		if (!error) {
+			for (var i = 0; i < tweets.length; i++) {
+				console.log(tweets[i].text);
+				}
+			} else {
+				console.log(error);
+			}
+	});
+
+};
+
+function doSomething() {
+	fs.appendFile("random.txt" , "," + userRequests);
+	spotify.search({type: "track", query: userRequests}, function(error, data){
+		console.log(data);
+	})
+}
+
